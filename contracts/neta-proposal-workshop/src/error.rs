1c533d22a9af813a41cdcc5149b9fa6079c9f4ef
cristianoneta <chris_fms@mailbox.org>
2026-09-16 22:11:52 +0200

    Add proposal workshop contract: contracts/neta-proposal-workshop/src/error.rs

---
+++ contracts/neta-proposal-workshop/src/error.rs
@@ -0,0 +1,37 @@
+use cosmwasm_std::StdError;
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+    #[error("unauthorized")]
+    Unauthorized,
+    #[error("contract is paused")]
+    Paused,
+    #[error("address is blocked")]
+    Blocked,
+    #[error("comment cooldown has {remaining_seconds} seconds remaining")]
+    Cooldown { remaining_seconds: u64 },
+    #[error("more than the configured NETA stake is required")]
+    CommentStakeNotMet,
+    #[error("DAO membership query failed")]
+    MembershipQueryFailed,
+    #[error("NETA stake query failed")]
+    StakeQueryFailed,
+    #[error("proposal is finalized")]
+    Finalized,
+    #[error("proposal is not finalized")]
+    NotFinalized,
+    #[error("version is not the latest revision")]
+    NotLatestVersion,
+    #[error("comment parent does not belong to this proposal")]
+    InvalidParent,
+    #[error("invalid {field} length; expected {min}..={max} characters")]
+    InvalidLength { field: String, min: usize, max: usize },
+    #[error("counter overflow")]
+    CounterOverflow,
+    #[error("funds are not accepted")]
+    FundsNotAccepted,
+}
+
